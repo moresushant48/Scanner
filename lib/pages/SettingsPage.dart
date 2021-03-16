02e41afec5e2507c6bd26f995b9ce4ce5ca1a5df
Sushant More <moresushant48@gmail.com>
2021-03-16 14:00:35 +0530

    Add SharedPreferences Service class & update settings page for night mode switch.
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- lib/pages/SettingsPage.dart
+++ lib/pages/SettingsPage.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:scanner/models/User.dart';
 import 'package:scanner/services/Auth.dart';
+import 'package:scanner/services/Prefs.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -21,97 +20,143 @@ class _SettingsPageState extends State<SettingsPage> {
     final _size = 18.0;
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Settings"),
-        ),
-        body: ListView(
-          children: [
-            FutureBuilder(
-              future: authService.signIn(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done &&
-                    snapshot.data.displayName != null)
-                  return Card(
-                    margin: EdgeInsets.all(_size),
-                    child: Container(
-                      padding: EdgeInsets.all(_size),
-                      width: MediaQuery.of(context).size.width,
-                      child: Column(
-                        children: [
-                          ClipPath(
-                            clipper: OvalBottomBorderClipper(),
-                            child: Image(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(snapshot.data.photoUrl),
-                              height: 100.0,
-                              width: 100.0,
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body: ListView(
+        children: [
+          StatefulBuilder(
+            builder: (context, setState) {
+              return FutureBuilder(
+                future: authService.signIn(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done &&
+                      snapshot.data.displayName != null)
+                    return Card(
+                      margin: EdgeInsets.all(_size),
+                      child: Container(
+                        padding: EdgeInsets.all(_size),
+                        width: MediaQuery.of(context).size.width,
+                        child: Column(
+                          children: [
+                            ClipPath(
+                              clipper: OvalBottomBorderClipper(),
+                              child: Image(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(snapshot.data.photoUrl),
+                                height: 100.0,
+                                width: 100.0,
+                              ),
                             ),
-                          ),
-                          //
-                          SizedBox(
-                            height: 14.0,
-                          ),
-                          //
-                          Text(
-                            snapshot.data.displayName,
-                            style: TextStyle(
-                                fontSize: 20.0, fontWeight: FontWeight.bold),
-                          ),
-                          //
-                          SizedBox(
-                            height: 10.0,
-                          ),
-                          //
-                          Text(
-                            snapshot.data.email,
-                            style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 14.0,
-                                fontWeight: FontWeight.normal),
-                          )
-                        ],
+                            //
+                            SizedBox(
+                              height: 14.0,
+                            ),
+                            //
+                            Text(
+                              snapshot.data.displayName,
+                              style: TextStyle(
+                                  fontSize: 20.0, fontWeight: FontWeight.bold),
+                            ),
+                            //
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            //
+                            Text(
+                              snapshot.data.email,
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.normal),
+                            )
+                          ],
+                        ),
                       ),
+                    );
+                  else
+                    return ListTile(
+                      contentPadding: EdgeInsets.all(_size),
+                      isThreeLine: true,
+                      leading: FaIcon(
+                        FontAwesomeIcons.google,
+                        color: Colors.green,
+                      ),
+                      title: Text("Sign In with Google ?"),
+                      subtitle: Text(
+                          "Connect to Google Account & Save docs on Drive."),
+                    );
+                },
+              );
+            },
+          ),
+          //
+
+          StatefulBuilder(
+            builder: (context, setState) => Column(
+              children: [
+                SizedBox(
+                  height: 4.0,
+                ),
+                //
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: _size),
+                  child: Text(
+                    "Security",
+                    style: TextStyle(
+                        color: Colors.redAccent, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                // fingerprint
+                FutureBuilder(
+                  future: prefService.getSharedBool("isFpOn"),
+                  builder: (context, snapshot) => SwitchListTile(
+                    title: Text("Use Fingerprint"),
+                    secondary: FaIcon(
+                      FontAwesomeIcons.fingerprint,
+                      color: Colors.indigo,
                     ),
-                  );
-                else
-                  return ListTile(
-                    contentPadding: EdgeInsets.all(_size),
-                    isThreeLine: true,
-                    leading: FaIcon(
-                      FontAwesomeIcons.google,
-                      color: Colors.green,
+                    value: snapshot.data,
+                    onChanged: (value) {
+                      setState(() {
+                        prefService.putSharedBool("isFpOn", value);
+                      });
+                    },
+                  ),
+                ),
+
+                //
+                SizedBox(
+                  height: 4.0,
+                ),
+                //
+                Text(
+                  "Theme",
+                  style: TextStyle(
+                      color: Colors.redAccent, fontWeight: FontWeight.bold),
+                ),
+
+                FutureBuilder(
+                  future: prefService.getSharedBool("isNightMode") ?? false,
+                  builder: (context, snapshot) => SwitchListTile(
+                    title: Text("Dark/Night Mode"),
+                    secondary: FaIcon(
+                      FontAwesomeIcons.solidMoon,
+                      color: Colors.indigo,
                     ),
-                    title: Text("Sign In with Google ?"),
-                    subtitle:
-                        Text("Connect to Google Account & Save docs on Drive."),
-                  );
-              },
+                    value: snapshot.data,
+                    onChanged: (value) {
+                      setState(() {
+                        prefService.putSharedBool("isNightMode", value);
+                      });
+                    },
+                  ),
+                ),
+              ],
             ),
-
-            //
-            SizedBox(
-              height: 4.0,
-            ),
-            //
-
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: _size),
-              child: Text(
-                "Security",
-                style: TextStyle(
-                    color: Colors.redAccent, fontWeight: FontWeight.bold),
-              ),
-            ),
-
-            ListTile(
-              contentPadding: EdgeInsets.all(_size),
-              leading: FaIcon(
-                FontAwesomeIcons.fingerprint,
-                color: Colors.indigo,
-              ),
-              title: Text("Use Fingerprint"),
-            ),
-          ],
-        ));
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/services/Prefs.dart
@@ -0,0 +1,68 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PrefService {
+  SharedPreferences _sharedPreferences;
+
+  Map<String, dynamic> defaults = {
+    "isNightMode": false,
+    "isFpOn": false,
+  };
+
+  //constructor
+  PrefService();
+
+  void putSharedBool(String key, dynamic value) async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+    _sharedPreferences.setBool(key, value);
+  }
+
+  void putSharedInt(String key, dynamic value) async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+    _sharedPreferences.setInt(key, value);
+  }
+
+  void putSharedDouble(String key, dynamic value) async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+    _sharedPreferences.setDouble(key, value);
+  }
+
+  void putSharedString(String key, dynamic value) async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+    _sharedPreferences.setString(key, value);
+  }
+
+  Future<bool> getSharedBool(String key) async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+    return _sharedPreferences.getBool(key) != null
+        ? _sharedPreferences.getBool(key)
+        : false;
+  }
+
+  Future<int> getSharedInt(String key) async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+    return _sharedPreferences.getInt(key) != null
+        ? _sharedPreferences.getInt(key)
+        : false;
+  }
+
+  Future<double> getSharedDouble(String key) async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+    return _sharedPreferences.getDouble(key) != null
+        ? _sharedPreferences.getDouble(key)
+        : false;
+  }
+
+  Future<String> getSharedString(String key) async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+    return _sharedPreferences.getString(key) != null
+        ? _sharedPreferences.getString(key)
+        : false;
+  }
+
+  getShared(String key) async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+    _sharedPreferences.get(key);
+  }
+}
+
+final prefService = PrefService();
