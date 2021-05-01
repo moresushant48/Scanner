08ae2d57294c7b1ef207462b4f2cc24b34cfbb01
Sushant More <moresushant48@gmail.com>
2021-05-01 12:32:53 +0530

    Cleanup
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,11 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import 'package:scanner/appbar/MainAppBar.dart';
 import 'package:scanner/pages/ScanPage.dart';
 import 'package:scanner/pages/ScansListWidget.dart';
-import 'package:scanner/services/Storage.dart';
 
 class HomePage extends StatefulWidget {
   @override
